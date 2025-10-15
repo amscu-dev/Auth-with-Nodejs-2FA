7b654c86da5c9d615acf5ef1c17fdc45e6c7f1ef
amscu-dev <amarinescu.dev@gmail.com>
2025-10-15 22:46:58 +0300

    server application config

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { config } from "./config/app.config";
+import connectDatabase from "./database/models/database";
+import { errorHandler } from "./middlewares/errorHandler";
+import { HTTPSTATUS } from "./config/http.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: config.APP_ORIGIN }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(HTTPSTATUS.OK).json({
+    message: "OK",
+  });
+});
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
+});
