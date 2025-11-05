5bf8d1d3d89ec9ea2e1739bdf2aeaaf0bada1d7d
amscu-dev <amarinescu.dev@gmail.com>
2025-11-05 17:12:26 +0200

    testing new openapi generators

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-full h-auto">
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full max-w-[450px] mx-auto h-auto ">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default AuthLayout;
--- client/lib/axios-client.ts
+++ client/lib/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const options = {
-  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
   timeout: 10000,
 };
@@ -10,14 +10,20 @@ const API = axios.create(options);
 
 API.interceptors.response.use(
   (response) => {
+    console.log(response.data);
     return response;
   },
   (error) => {
+    console.log(error);
+    console.log(error.response);
     const { data, status } = error.response;
     if (data === "Unauthorized" && status === 401) {
+      // retry pe endpoint ul de /refrehs
     }
     return Promise.reject({
       ...data,
     });
   }
 );
+
+export default API;
