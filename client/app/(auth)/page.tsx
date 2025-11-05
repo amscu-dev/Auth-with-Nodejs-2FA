3b5bd0100f17d82867a227d715a1df2dc4487a9f
amscu-dev <amarinescu.dev@gmail.com>
2025-11-05 14:51:59 +0200

    Added basic frontend template

---
+++ client/app/(auth)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <Button variant="default" size="lg">
+        Default
+      </Button>
+    </div>
+  );
+}
