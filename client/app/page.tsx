474f0b0716acdb135d6d5159a0b413602b806f41
amscu-dev <amarinescu.dev@gmail.com>
2025-11-15 22:43:36 +0200

    Finish Home Screen UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,17 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import { MdArrowOutward } from "react-icons/md";
+import { Separator } from "@/components/ui/separator";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaAws } from "react-icons/fa";
+import { BsFillShieldLockFill } from "react-icons/bs";
+import { FaKey } from "react-icons/fa";
+import { FaFingerprint } from "react-icons/fa";
 const HomePage = () => {
   return (
     <div className="w-full h-full overflow-hidden relative flex items-center justify-center">
@@ -14,30 +25,62 @@ const HomePage = () => {
                bg-[radial-gradient(ellipse_at_center,#00000000_35%,#ffffff_100%)] z-10 dark:bg-[radial-gradient(ellipse_at_center,#00000000_35%,#000000_100%)]"
       />
       <div className="flex items-center justify-center flex-col relative z-30">
-        <h1 className="text-5xl font-bold tracking-tighter mb-8 text-center">
+        <h1 className="text-5xl font-bold tracking-tighter mb-8 text-center slide-in-from-top">
           Strong{" "}
           <span className="text-primary bg-white shadow-md px-4 py-2 inline-block rounded-[12px]">
             Security
           </span>{" "}
           refined into a Simple <span className="text-primary">Experience</span>
         </h1>
-        <p className="text-lg text-muted-foreground font-medium font-mono text-center">
+        <p className="text-lg text-muted-foreground font-medium font-mono text-center slide-in-from-right opacity-0">
           A hands-on look at today’s most frequent authentication methods
         </p>
-        <p className="text-lg mb-24 text-muted-foreground font-medium font-mono text-center">
+        <p className="text-lg mb-24 text-muted-foreground font-medium font-mono text-center slide-in-from-left opacity-0">
           {" "}
           from passkeys to session management and multi-factor, all in one
           interactive demo.
         </p>
         <Button
           asChild
-          className="px-12 text-lg h-12 relative"
+          className="px-12 text-lg h-12 relative slide-in-from-bottom"
           variant="outline"
         >
           <Link href="/accounts/signin">
             Launch App <MdArrowOutward className="absolute right-6 text-3xl" />
           </Link>
         </Button>
+        {/* ICONS ON SCREEN */}
+        <div
+          className="flex items-center justify-center p-4 absolute border bg-amber-400 shadow-lg rounded-full rotate-12 -bottom-[25%] right-[5%] 
+         opacity-0 to-opacity-100"
+        >
+          <BsFillShieldLockFill className="text-6xl text-purple-600 " />
+        </div>
+        <div
+          className="flex items-center justify-center p-3 absolute border bg-indigo-800 shadow-lg  rounded-[5px] rotate-12 -top-[40%] right-[20%] 
+         opacity-0 to-opacity-100"
+        >
+          <FaFingerprint className="text-5xl text-zinc-200" />
+        </div>
+        <div
+          className="flex items-center justify-center p-6 absolute border bg-sky-800 shadow-lg rounded-[20px] -rotate-12 bottom-[3%] -left-[7%] 
+         opacity-0 to-opacity-100"
+        >
+          <FaKey className="text-6xl text-orange-600 " />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center absolute bottom-4 right-1/2 translate-x-1/2 opacity-0 to-opacity-100">
+        <h2 className="mb-2 font-mono font-semibold text-sm">Tech Stack</h2>
+        <Separator className="mb-3 bg-border/80 w-[150%]" />
+        <div className="flex items-center justify-center gap-6 z-50">
+          <RiNextjsFill size="1.75rem" />
+          <SiMongodb size="1.75rem" />
+          <FaNodeJs size="1.75rem" />
+          <SiExpress size="1.75rem" />
+          <SiTypescript size="1.75rem" />
+          <RiTailwindCssFill size="1.75rem" />
+          <FaAws size="1.75rem" />
+        </div>
       </div>
     </div>
   );
--- client/components/links-bar/LinksBar.tsx
+++ client/components/links-bar/LinksBar.tsx
@@ -4,21 +4,46 @@ import { FaGithubAlt } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { IoCodeSlashSharp } from "react-icons/io5";
 import ToggleThemeButton from "./ToggleThemeButton";
+import { HiViewGridAdd } from "react-icons/hi";
 
 interface LinksBarProps {}
 
 const LinksBar: React.FC<LinksBarProps> = () => {
   return (
-    <div className="absolute top-2 right-4  flex items-center justify-center gap-4 z-50">
+    <div className="absolute top-2 right-4 left-4 flex items-center justify-center gap-4 z-50 slide-in-from-top">
+      <Button
+        asChild
+        size="sm"
+        className="rounded-[9px]  mr-auto gap-0"
+        variant="outline"
+      >
+        <Link href="/">
+          <HiViewGridAdd className="mr-2" />
+          <span>Login</span>
+          <span className="text-primary font-extrabold">Sandbox</span>
+        </Link>
+      </Button>
+      <Button
+        asChild
+        size="sm"
+        className="w-28 rounded-[9px] bg-teal-500 text-white"
+        variant="outline"
+      >
+        <Link href="https://docs.loginsandbox.xyz">
+          <IoCodeSlashSharp />
+          <span>API Docs</span>
+        </Link>
+      </Button>
       <Button
         asChild
         size="sm"
         className="w-28 bg-accent rounded-[9px] hover:bg-blue-400"
       >
         <Link href="https://www.linkedin.com/in/amscu/">
-          <span>amscu</span>
           <FaLinkedin />
+          <span>amscu</span>
         </Link>
       </Button>
       <Button
@@ -28,10 +53,11 @@ const LinksBar: React.FC<LinksBarProps> = () => {
         variant="outline"
       >
         <Link href="https://github.com/amscu-dev">
-          <span>amscu-dev</span>
           <FaGithubAlt />
+          <span>amscu-dev</span>
         </Link>
       </Button>
+
       <ToggleThemeButton />
     </div>
   );
