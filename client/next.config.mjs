1f7dc550db99d47a11bd71233ccf76aebd10514a
amscu-dev <amarinescu.dev@gmail.com>
2025-11-17 16:48:18 +0200

    modified security headears in client

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,7 +8,11 @@ jiti("./env");
 
 const cspHeader = `
   default-src 'self';
-  connect-src 'self' http://localhost:8000;
+  connect-src 'self' ${
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000"
+      : "https://api.loginsandbox.xyz"
+  };
   script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src 'self' blob: data:;
@@ -17,7 +21,7 @@ const cspHeader = `
   base-uri 'self';
   form-action 'self';
   frame-ancestors 'none';
-  
+  ${process.env.NODE_ENV === "production" ? "upgrade-insecure-requests;" : ""}
 `;
 
 const nextConfig = {
@@ -42,6 +46,13 @@ const nextConfig = {
           {
             key: "Referrer-Policy",
             value: "origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value:
+              process.env.NODE_ENV === "production"
+                ? "max-age=63072000; includeSubDomains; preload"
+                : "",
           },
           {
             key: "Content-Security-Policy",
